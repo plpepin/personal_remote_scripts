5f0f619ee313c9382467a8f1f649510579575ad8
Pierre-Luc Pepin <work@whiteoctop.us>
2020-02-18 16:19:15 -0500

    Create scroll-and-assign-class.js
    Adds a class to a soundlist_item

---
+++ soundcloud/stream/scroll-and-assign-class.js
@@ -0,0 +1,21 @@
+var node_html = document.documentElement,
+    nodeList_scItems = node_html.querySelectorAll('.soundList__item');  // All first 10 items
+
+// Cycle thru all 10 items
+nodeList_scItems.forEach( (item) => {
+    
+    // Get the user who posted current item (track) to the stream..
+    username = item.querySelector('.soundContext__usernameLink').text;
+    
+    // Create a classname using the user url page minus the '/' prefix 
+    username-handle = item.querySelector('.g-avatar-badge-avatar-link').pathname.substring(1);
+    
+    // Assign new class to parent (item)
+    item.classList.add('magicianonduty');
+
+});
+
+// Scroll page to get 10 more items. We grab the height of the entire and scroll by that amount.. 
+// intListHeight = nodeList_scItems.scrollHeight;
+// node_html.scrollBy(0, intListHeight)
+// returns integer
