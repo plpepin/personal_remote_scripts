7cd7715f88ec0ff3bee7d1c0a9052227aa5f0468
Pierre-Luc Pepin <work@whiteoctop.us>
2020-02-18 19:33:15 -0500

    Debugging the script

--- soundcloud/stream/scroll-and-assign-class.js
+++ soundcloud/stream/scroll-and-assign-class.js
@@ -10,7 +10,7 @@ nodeList_scItems.forEach( (item) => {
     // Create a classname using the user url page minus the '/' prefix 
     username_handle = item.querySelector('.g-avatar-badge-avatar-link').pathname.substring(1);
     
-    console.log(username_handle);
+    console.log("username_handle class", username_handle);
     
     // Assign new class to parent (item)
     item.classList.add( username_handle );
